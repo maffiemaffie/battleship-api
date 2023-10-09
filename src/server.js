a865a2ceee4e32eafee71dfd5b2d4bbc59d8369c
Elia Cohen <mrc6113@rit.edu>
2023-10-09 01:46:54 -0400

    setup server.js and draft api handlers

--- src/server.js
+++ src/server.js
@@ -1,17 +1,54 @@
-// const http = require('http');
-// const url = require('url');
+const http = require('http');
+const url = require('url');
+const query = require('querystring');
 
-const bts = require('./battleship.ts');
+const jsonHandler = require('./jsonResponses.js');
+const htmlHandler = require('./htmlResponses.js');
 
-const game = new bts.BattleshipGame();
-const battleship1 = [
-  { row: 0, column: 1 },
-  { row: 0, column: 2 },
-  { row: 0, column: 3 },
-];
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-const battleship2 = [
-  { row: 1, column: 1 },
-  { row: 1, column: 2 },
-  { row: 1, column: 3 },
-];
+const urlStruct = {
+  GET: {
+    '/': htmlHandler.getIndex,
+    '/style.css': htmlHandler.getCSS,
+    '/boardTemplate': jsonHandler.getBoardTemplate,
+    '/battleshipTemplate': jsonHandler.getBattleshipTemplate,
+    '/status': jsonHandler.getStatus,
+    '/data': jsonHandler.getGameData,
+    notFound: jsonHandler.getNotFound,
+  },
+  HEAD: {
+    '/boardTemplate': jsonHandler.getBoardTemplateMeta,
+    '/battleshipTemplate': jsonHandler.getBattleshipTemplateMeta,
+    '/status': jsonHandler.getStatusMeta,
+    '/data': jsonHandler.getGameDataMeta,
+    notFound: jsonHandler.getNotFoundMeta,
+  },
+  POST: {
+    '/new': jsonHandler.createGame,
+    '/join': jsonHandler.joinGame,
+    '/ready': jsonHandler.setBattleships,
+    '/shoot': jsonHandler.shootTarget,
+    notFound: jsonHandler.getNotFoundMeta,
+  },
+};
+
+const onRequest = (request, response) => {
+  const parsedUrl = url.parse(request.url);
+  const params = query.parse(parsedUrl.query);
+
+  if (!urlStruct[request.method]) {
+    return urlStruct.HEAD.notFound(request, response);
+  }
+
+  if (urlStruct[request.method][parsedUrl.pathname]) {
+    return urlStruct[request.method][parsedUrl.pathname](request, response, params);
+  }
+
+  return urlStruct[request.method].notFound(request, response);
+}
+
+http.createServer(onRequest).listen(port, () => {
+  console.log(`Listening on 127.0.0.1:${port}`);
+});
+
