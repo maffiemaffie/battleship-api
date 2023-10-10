a17110a6df15830c67f279ab4ba84a90eb834dca
Elia Cohen <mrc6113@rit.edu>
2023-10-09 20:11:20 -0400

    added filepaths

--- src/htmlResponses.js
+++ src/htmlResponses.js
@@ -1,5 +1,9 @@
 const fs = require('fs');
 
+const index = fs.readFileSync(`${__dirname}/../client/client.html`);
+const css = fs.readFileSync(`${__dirname}/../client/style.css`);
+const indexjs = fs.readFileSync(`${__dirname}/../client/index.js`);
+
 /**
  * Gets the index html page.
  * @param {http.IncomingMessage} request
@@ -24,7 +28,7 @@ const getCSS = (request, response) => {
 
 const getIndexJS = (request, response) => {
   response.writeHead(200, { 'Content-Type': 'application/js' });
-  response.write(css);
+  response.write(indexjs);
   response.end();
 }
 
